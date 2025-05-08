82f92aeae6874f4f373ab454ae8cf468d08443e7
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-05-08 20:54:39 +0700

    Menambahkan class TodoLoaded sebagai state data berhasil dimuat, berisi list todo dan tanggal

--- lib/bloc/todo_state.dart
+++ lib/bloc/todo_state.dart
@@ -5,3 +5,12 @@ sealed class TodoState {}
 final class TodoInitial extends TodoState {}
 
 final class TodoLoading extends TodoState {}
+
+final class TodoLoaded extends TodoState {
+  final List<Todo> todos;
+  DateTime? selectedDate;
+
+  TodoLoaded({
+    required this.todos, required this.selectedDate
+  });
+}
