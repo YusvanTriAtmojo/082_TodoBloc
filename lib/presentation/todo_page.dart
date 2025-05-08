a171137383549e334ebc2294bdab0e51355180a4
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-05-08 21:23:09 +0700

    Membuat tampilan yang menampilkan tanggal yang dipilih melalui BlocBuilder

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/bloc/todo_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
@@ -8,6 +10,37 @@ class TodoPage extends StatelessWidget {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
     return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('Selected Date'),
+                      BlocBuilder<TodoBloc, TodoState>(
+                        builder: (context, state) {
+                          if (state is TodoLoaded) {
+                            if (state.selectedDate != null) {
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text('No date selected');
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
